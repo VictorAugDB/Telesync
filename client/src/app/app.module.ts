3c35269989ca91b7027a75eb911840770afde9ae
Victor Augusto <vdbernardes@gmail.com>
2020-10-11 11:34:59 -0300

    Adicionado angular-Material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/template/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { HeaderComponent } from './component/template/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
