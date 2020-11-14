ce6136ae41f6fa5641b4b3264a7f2c10a8e345d6
Victor Augusto <vdbernardes@gmail.com>
2020-11-14 08:24:50 -0300

    Adicionado busca a table vendas

--- client/src/app/component/servicos/listar-vendas/listar-vendas.component.ts
+++ client/src/app/component/servicos/listar-vendas/listar-vendas.component.ts
@@ -1,3 +1,4 @@
+import { Cliente } from './../../cliente/client.model';
 import { ProductService } from './../product.service';
 import { Venda } from './../models/product-venda.model';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
@@ -6,6 +7,7 @@ import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { ListarVendasDataSource } from './listar-vendas-datasource';
 import { ClientService } from '../../cliente/client.service';
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
   selector: 'app-listar-vendas',
@@ -16,24 +18,45 @@ export class ListarVendasComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<Venda>;
-  dataSource: ListarVendasDataSource = null;
+  dataSource = new MatTableDataSource();
+
+  cliente: Cliente = null;
+
+  vendas: Venda[] = [{
+    quantidadeChips: null,
+    dtVenda: '',
+    dtVencimento: '',
+    valorTotal: null,
+    obs: 'a',
+    formaPagamento: '',
+    statusPagamento: null,
+    cliente: this.cliente
+  }]
+
+  //dataSource: ListarVendasDataSource = null;
 
   constructor(private productService: ProductService, private clientService: ClientService) {
   }
-
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['codVenda', 'quantidadeChips', 'dtVenda', 'dtVencimento', 'valorTotal', 'formaPagamento', 'statusPagamento', 'action'];
 
   ngOnInit() {
-    this.dataSource = new ListarVendasDataSource(this.productService, this.clientService);
-    
+
+    this.productService.buscarVendasCliente(1).subscribe(vendas => {
+      this.vendas = vendas;
+      this.dataSource = new MatTableDataSource(vendas)
+    })
   }
 
   ngAfterViewInit() {
     setTimeout(() => {
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
-      this.table.dataSource = this.dataSource;
+      this.dataSource = this.dataSource;
     }, 500)
   }
+
+  applyFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
